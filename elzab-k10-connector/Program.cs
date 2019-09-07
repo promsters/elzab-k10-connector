5578172120f6c7272fecd6512d0b52c94d5d0336
tomasz.ptak <tomasz.ptak@sklepopon.com>
2019-09-07 18:23:16 +0200

    init

---
+++ elzab-k10-connector/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO.Ports;
+
+namespace ElzabK10Connector
+{
+    class Program
+    {
+        //deklaracja funkcji z dwoma parametrami
+        [DllImport("WinIP.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern byte __DrukRapD(string inputfile, string outputfile);
+
+        static int Main(string[] args)
+        {
+            SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+
+            port.WriteLine("123456");
+
+            return 1;
+        }
+
+        static int Mainxd(string[] args)
+        {
+            byte error;
+
+            //wywołanie funkcji z dwoma parametrami
+            error = __DrukRapD("drukrapd.in", "drukrapd.out");
+            if (error != 0)
+            {
+                Console.WriteLine(error);
+                Console.In.ReadLine();
+                //obsługa błędów
+                return error;
+            }
+
+            return error;
+        }
+    }
+}
---
+++ elzab-k10-connector/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("przyklad")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ELZAB")]
+[assembly: AssemblyProduct("przyklad")]
+[assembly: AssemblyCopyright("Copyright © ELZAB 2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45a47bf8-31f5-41c7-aef1-32e1e9e2814c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
